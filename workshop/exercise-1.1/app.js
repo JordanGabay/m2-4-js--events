4b901b9accfa9e684bdc51fca4919c4b638151f6
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-18 14:03:35 -0400

    Exercise 1.1 Complete

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,21 @@
 // Exercise 1.1
 // ------------
 console.log('exercise 1.1');
+let state = false;
+let result = document.querySelector('#result');
+
+let clickEvent = function() {
+    state = true;
+}
+
+let checkforwinner = function() {
+    result.innerHTML = state ? 'You Win': 'You Lose';
+    // if (state ) 
+    //     result.innerHTML = 'you win' 
+    // else
+    //     result.innerHTML = 'you lose'
+}
+
+let timer = setTimeout(checkforwinner, 1000);
+
+document.addEventListener('click', clickEvent);
