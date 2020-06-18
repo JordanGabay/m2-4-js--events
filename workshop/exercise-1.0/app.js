276734dc758510e746e1ccc0db681c6d9f251243
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-18 14:03:14 -0400

    Exercise 1.0 Complete

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,13 @@
 // Exercise 1.0
 // ------------
 console.log('exercise-1');
+
+const body = document.querySelector('body');
+const result = document.getElementById('result');
+
+let clickEvent = function() {
+    result.innerText ="Good Job!";
+    body.removeEventListener('click', clickEvent);
+}
+
+body.addEventListener('click', clickEvent);
